666f1b45fe1ec1ec5a005d5076ef74dc87ff8a44
thanawat.bcr <thanwat.bcr@gmail.com>
2022-05-04 22:01:48 +0700

    [ADD] navigation and color

--- components/header/MainNav.vue
+++ components/header/MainNav.vue
@@ -4,20 +4,20 @@ header
     .container.flex.flex-col.gap-y-4
       //- Top Nav
       .flex.gap-x-8.justify-end
-        span.flex.items-center.gap-x-2
-          i.ph-bell.text-lg.text-gray-500
-          .text-xs.text-gray-500 การแจ้งเตือน
-        span.flex.items-center.gap-x-2
-          i.ph-phone.text-lg.text-gray-500
-          .text-xs.text-gray-500 ติดต่อเรา
-        span.flex.items-center.gap-x-2
-          i.ph-globe.text-lg.text-gray-500
-          .text-xs.text-gray-500 ไทย
+        span.flex.items-center.gap-x-2.cursor-pointer.text-gray-500(:class="'hover:text-gray-600'")
+          i.ph-bell.text-lg
+          .text-xs การแจ้งเตือน
+        span.flex.items-center.gap-x-2.cursor-pointer.text-gray-500(:class="'hover:text-gray-600'")
+          i.ph-phone.text-lg
+          .text-xs ติดต่อเรา
+        span.flex.items-center.gap-x-2.cursor-pointer.text-gray-500(:class="'hover:text-gray-600'")
+          i.ph-globe.text-lg
+          .text-xs ไทย
         SoButton(size="sm") ลงชื่อเข้าใช้
 
       //- Main Nav
       .grid-container.items-center
-        img.col-span-2(src="/logo/logo-color.svg")
+        NuxtLink(to="/").col-span-2: img.cursor-pointer(src="/logo/logo-color.svg")
         .flex.flex-col.gap-y-3.col-span-9
           SoInput(
             v-model="search"
@@ -25,12 +25,12 @@ header
             placeholder="อาหารสัตว์เลี้ยง, อุปกรณ์สำหรับสัตว์เลี้ยง หรือ อื่นๆ"
           )
           .flex.gap-x-6
-            .text-xs.text-gray-400(v-for="pet in pets" :key="pet") {{ pet }}
-        .flex.flex-col.items-center.col-span-1
+            .text-xs.text-gray-400.cursor-pointer(:class="'hover:text-gray-600'" v-for="pet in pets" :key="pet") {{ pet }}
+        .flex.flex-col.items-center.col-span-1.cursor-pointer.text-gray-500(:class="'hover:text-gray-600'")
           span.relative
             .text-xs.font-bold.absolute.text-white.bg-orange-900.px-2.rounded-lg.left-4(v-if="cartCount > 0") {{ cartCount }}
-            i.ph-shopping-cart-simple.text-2xl.text-gray-500
-          .text-sm.text-gray-500 รถเข็น
+            i.ph-shopping-cart-simple.text-2xl
+          .text-sm รถเข็น
         //- .flex.flex-col.items-center.col-span-1
           i.ph-ticket.text-2xl.text-gray-500
           .text-sm.text-gray-500 ส่วนลด
@@ -44,7 +44,7 @@ const MainNav = defineComponent({
   setup() {
     const pets = reactive(['สุนัข','แมว','ปลาและสัตว์น้ำ','นก','เป็ด','ห่าน','กระต่าย','เม่น','ม้า','เต่า','หนู','กระรอก','หมู',]);
 
-    const cartCount = ref(0);
+    const cartCount = ref(2);
 
     const search = ref('');
 
