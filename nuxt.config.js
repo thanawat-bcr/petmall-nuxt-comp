c0e30d8312a89e2c81e824d3e00aa59d69d2fe19
thanawat.bcr <thanwat.bcr@gmail.com>
2022-05-04 20:45:48 +0700

    [ADD] styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "@/assets/css/typography.scss",
+    "@/assets/css/style.scss",
   ],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
