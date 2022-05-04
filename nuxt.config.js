8fd362001d83d9d61cf67a7032b93dd2c8f19c6a
thanawat.bcr <thanwat.bcr@gmail.com>
2022-05-04 19:34:29 +0700

    [ADD] petmall logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'petmall-client',
+    title: 'PetMall',
     htmlAttrs: {
       lang: 'en'
     },
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/logo/logo-color.svg' }
     ],
     script: [
       { src: "https://unpkg.com/phosphor-icons" },
