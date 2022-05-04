a41d63f37cf9d5a22447f0f7896ac37a8d6f7a0a
thanawat.bcr <thanwat.bcr@gmail.com>
2022-05-04 19:49:47 +0700

    [CLEAN]

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,17 @@
 <template lang="pug">
-.index.bg-green-200.w-screen.h-screen
-  //- .flex.items-center.justify-center.w-8.h-8.bg-alert-900
-    i.ph-smiley.text-xl.text-white
-  //- .flex.flex-col.gap-y-4
-    SoButton(type="main" size="md" leading="plus" trailing="arrow-right") Button
-    SoButton(type="soft" size="md" leading="plus" trailing="arrow-right" block) Button
-    SoButton(type="outline" size="md" leading="plus" trailing="arrow-right") Button
-    SoButton(type="text" size="md" leading="plus" trailing="arrow-right") Button
-  //- Vee Validate Tester
-  //- ValidationObserver
-    ValidationProvider(rules="required|email" v-slot="{ errors }")
-      input.bg-green-200(v-model="user.email" type="email" name="email" placeholder="email")
-      span {{ errors[0] }}
-  .flex.flex-col.p-4
-    SoInput(
-      v-model="user.email"
-      placeholder="email"
-      rules="required"
-      type="password"
-      disabled
-    )
-    SoInput(
-      v-model="user.email"
-      placeholder="email"
-      leading="smiley"
-      rules="required|email"
-    )
-    SoInput(
-      v-model="user.email"
-      placeholder="email"
-      trailing="arrow-right"
-      rules="required|email"
-    )
-    SoInput(
-      v-model="user.email"
-      placeholder="email"
-      leading="smiley"
-      trailing="arrow-right"
-      rules="required|email"
-    )
-  //- CommonForm(@submit="submit")
-    .form--group
-      .form--row
-        CommonInput(
-          v-model="user.email"
-          :icon="'/icons/mail.svg'"
-          placeholder="Email"
-          rules="required|email"
-        ) Email
-        CommonInput(
-          v-model="user.password"
-          placeholder="Password"
-          rules="required"
-          type="password"
-        ) Password
-      .form--row
-        input(type="submit" value="Submit")
+.index.bg-green-900.w-screen.h-screen
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from '@nuxtjs/composition-api';
+import { defineComponent } from '@nuxtjs/composition-api';
 
 const index = defineComponent({
-  setup() {
-    const user = reactive({
-      email: '',
-      password: '',
-    });
-
-    const submit = () => {
-      console.log(user);
-    };
-
-    return {
-      user,
-      submit,
-    };
-  },
+  // setup() {},
 });
 
 export default index;
 </script>
 
 <style lang="scss">
-.index {
-  @apply container mx-auto;
-}
+// .index {}
 </style>
