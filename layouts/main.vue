c53d6451d50117b58de445de05ad7d88ab76d8ef
thanawat.bcr <thanwat.bcr@gmail.com>
2022-05-04 08:11:10 +0700

    [TEST] layout

---
+++ layouts/main.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.main
+  h1 THIS IS MAIN LAYOUTs
+  Nuxt
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+const main = defineComponent({
+  // setup() {},
+});
+
+export default main;
+</script>
+
+<style lang="scss">
+// .main {}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.about
+  h3 about page
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+const about = defineComponent({
+  layout: 'main',
+  // setup() {},
+});
+
+export default about;
+</script>
+
+<style lang="scss">
+// .about {}
+</style>
