3e2dd61975a85deacdba47d96b7250affccc71d0
thanawat.bcr <thanwat.bcr@gmail.com>
2022-05-10 12:05:34 +0700

    [FIX] width

--- components/header/PrimaryNav.vue
+++ components/header/PrimaryNav.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 header
-  nav.primary-nav.w-screen.flex.items-center.shadow-01(:class="navbarColor.bg")
+  nav.primary-nav.flex.items-center.shadow-01(:class="navbarColor.bg")
     .container.flex.flex-col.gap-y-4
       //- Top Nav
       .flex.gap-x-8.justify-end(:class="navbarColor.text")
