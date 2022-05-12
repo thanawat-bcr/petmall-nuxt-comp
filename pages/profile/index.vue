4dd5f9570b420e28cb3fc1a117dbe9b5abc820b0
thanawat-bcr <thanawat.bcr@gmail.com>
2022-05-12 19:43:22 +0700

    [ADD] profile template

---
+++ pages/profile/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.index
+  h1 Profile
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+const index = defineComponent({
+  layout: 'primary',
+  mounted() {
+    this.$nuxt.$emit('setLayout', { color: true, profile: true, });
+  },
+  // setup() {},
+});
+
+export default index;
+</script>
+
+<style lang="scss" scoped>
+// .index {}
+</style>
