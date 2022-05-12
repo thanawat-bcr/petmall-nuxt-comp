13819da85e43f7c4cd92c2449880d7b8d595d099
thanawat-bcr <thanawat.bcr@gmail.com>
2022-05-12 19:43:14 +0700

    [ADD] sidenav layout

---
+++ components/header/ProfileSidenav.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+aside.profile-sidenav.bg-gray-100.w-full.h-full.p-6.flex.flex-col.gap-y-6
+  .flex.items-center.gap-x-2
+    .w-14.h-14.rounded-full.bg-gray-200.flex.items-center.justify-center: i.ph-user.text-2xl.text-gray-400
+    .flex.flex-col.gap-y-1
+      .text-sm.font-bold.text-gray-500.en Tutorism
+      .flex.items-end.gap-x-1.cursor-pointer
+        i.ph-pencil-simple.text-xl.text-gray-500
+        .button-sm.text-gray-500 แก้ไขบัญชีของคุณ
+  .flex.flex-col.gap-y-4
+    .flex.gap-x-2.items-center.cursor-pointer(@click="menuIndexHandler(0)")
+      i.ph-gear.text-2xl.text-gray-500
+      .button-sm.font-bold.text-gray-500 ตั้งค่าบัญชีของฉัน
+    .flex.flex-col.gap-y-4(v-if="menuIndex === 0")
+      .text-sm.text-gray-500.cursor-pointer ข้อมูลส่วนตัว
+      .text-sm.text-gray-500.cursor-pointer ที่อยู่
+      .text-sm.text-gray-500.cursor-pointer เปลี่ยนรหัสผ่าน
+
+  .flex.flex-col.gap-y-4 
+    .flex.gap-x-2.items-center.cursor-pointer(@click="menuIndexHandler(1)" :class="`${menuIndex === 1 ? 'text-green-800' : 'text-gray-500'}`")
+      i.ph-heart.text-2xl
+      .button-sm.font-bold รายการที่ชอบ
+
+  .flex.flex-col.gap-y-4
+    .flex.gap-x-2.items-center.cursor-pointer(@click="menuIndexHandler(2)" :class="`${menuIndex === 2 ? 'text-green-800' : 'text-gray-500'}`")
+      i.ph-notepad.text-2xl
+      .button-sm.font-bold การสั่งซื้อของฉัน
+</template>
+
+<script lang="ts">
+import { defineComponent, Ref, ref } from '@nuxtjs/composition-api';
+
+const ProfileSidenav = defineComponent({
+  setup() {
+    const menuIndex: Ref<Number> = ref(0);
+
+    const menuIndexHandler = (index: Number) => { menuIndex.value = index; };
+
+    return {
+      menuIndex,
+      menuIndexHandler,
+    }
+  },
+});
+
+export default ProfileSidenav;
+</script>
+
+<style lang="scss" scoped>
+// .profile-sidenav {}
+</style>
