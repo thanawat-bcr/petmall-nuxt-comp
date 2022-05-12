55dc0dc6b5275c025d42b11b9da5709a32b092c6
thanawat-bcr <thanawat.bcr@gmail.com>
2022-05-12 21:45:51 +0700

    [ADD] basic radio

---
+++ components/SoRadio.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+label.so-radio.inline-flex.items-center.gap-x-2.cursor-pointer
+  input.hidden(
+    type="radio"
+    :checked="selected"
+    @input="$emit('select', value)"
+  )
+  .w-5.h-5.border-gray-200.border.border-solid.rounded-full(v-if="!selected")
+  .w-5.h-5.border-green-800.border.border-solid.rounded-full.flex.items-center.justify-center(v-else)
+    .w-3.h-3.bg-green-800.rounded-full
+  .text-sm.text-gray-500: slot
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+const SoRadio = defineComponent({
+  props: {
+    value: {},
+    selected: { type: Boolean, required: true },
+  },
+  // setup() {},
+});
+
+export default SoRadio;
+</script>
+
+<style lang="scss" scoped>
+// .so-radio {}
+</style>
---
+++ pages/dev/radio.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+.radio
+  .container
+    .flex.flex-col.gap-y-2.mt-8
+      SoRadio(
+        v-for="(op, index) in options"
+        :key="op.value"
+        v-model="index"
+        :selected="index === selectedIndex"
+        @select="(index) => selectedIndex = index"
+      ) {{ op.name }}
+
+    .my-8 
+
+    .text-alert-700.my-2(
+      v-for="op in options"
+    ) {{ op }}
+
+    .my-8 
+
+    .text-green-700.my-2 {{ options[selectedIndex] }}
+  
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from '@nuxtjs/composition-api';
+
+const radio = defineComponent({
+  layout: 'primary',
+  setup() {
+    const selectedIndex = ref(0);
+    
+    // VALUE -> return
+    // NAME -> LABEL [OPTIONAL]
+    const options = reactive([
+      { value: 'Value: 0', name: 'Choice 0' },
+      { value: 'Value: 1', name: 'Choice 1' },
+      { value: 'Value: 2', name: 'Choice 2' },
+      { value: 'Value: 3', name: 'Choice 3' },
+      { value: 'Value: 4', name: 'Choice 4' },
+    ]);
+
+    const selectHandler = (index: any) => { selectedIndex.value = index; };
+
+    return { selectedIndex, selectHandler, options };
+  },
+});
+
+export default radio;
+</script>
+
+<style lang="scss" scoped>
+// .radio {}
+</style>
