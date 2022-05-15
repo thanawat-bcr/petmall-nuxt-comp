837ad4c6e0543d9f009ce8ec4e142370ff9100a0
thanawat.bcr <thanwat.bcr@gmail.com>
2022-05-15 21:46:14 +0700

    [ADD] purchase with mock items

---
+++ components/product/Banner.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+.banner.w-full.flex.flex-col.gap-y-3.p-4.bg-gray-opacity-12.rounded-lg
+  //- HEADER
+  .flex.items-center.justify-between
+    .flex.gap-x-3
+      .h-6.rounded.px-5.bg-orange-900.flex.items-center.justify-center: .text-xs.text-gray-50.font-bold.text-center Featured
+      .text-sm.font-bold.text-gray-500 {{ item.shopName }}
+    .flex.gap-x-3.items-center(v-if="!noStatus")
+      .h-6.w-px.bg-gray-200
+      .text-sm.text-green-900 {{ item.status }}
+
+  .line.w-full.h-px.bg-gray-200
+
+  //- MAIN SECTION
+  .flex.gap-x-4
+    img.w-20.h-20.object-contain(src="/product/item/01.png")
+    .flex.flex-col.gap-y-1.flex-1
+      .text-xs.text-gray-500.flex-1 {{ item.name }}
+      .flex.gap-x-1
+        .text-xs.text-gray-400 ตัวเลือกสินค้า:
+        .text-xs.text-gray-400 {{ item.option }}
+      .text-xs.text-gray-400 {{ item.amount }}
+    .text-md(:class="`${noStatus ? 'text-orange-800 font-bold' : 'text-gray-500'}`") ฿{{ item.price }}
+
+  .line.w-full.h-px.bg-gray-200(v-if="!noStatus")
+
+  .flex.items-center.justify-between(v-if="!noStatus")
+    .flex.gap-x-1.items-center
+      .text-xs.text-gray-500 จัดส่งสินค้าภายในวันที่ : 
+      .text-xs.text-gray-500 {{ item.date }}
+    .flex.gap-x-1.items-center
+      .text-sm.text-gray-500.mr-4 ยอดคำสั่งซื้อทั้งหมด : 
+      .text-orange-900.text-md.font-bold ฿{{ item.price * item.amount }}
+
+  .flex.justify-end.gap-x-2(v-if="!noStatus && item.status === 'สำเร็จแล้ว'")
+    SoButton(size="md" mode="outline") ซื้ออีกครั้ง
+    SoButton(size="md") ให้คะแนน
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+const Banner = defineComponent({
+  props: {
+    item: {},
+    noStatus: { type: Boolean, default: false }
+  },
+  // setup() {},
+});
+
+export default Banner;
+</script>
+
+<style lang="scss" scoped>
+// .banner {}
+</style>
--- pages/profile/purchase.vue
+++ pages/profile/purchase.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
-.purchase.flex.flex-col.gap-y-8
+.purchase.flex.flex-col.gap-y-4
+
   .flex.flex-col.gap-y-2
     h4.text-gray-500 การสั่งซื้อของฉัน
+
   .grid.grid-cols-9.gap-x-4
     .tab-selector.button-sm(
       v-for="(title, index) in tabs"
@@ -17,10 +19,16 @@
       .flex.flex-col.gap-y-2.justify-center.text-center
         h6.text-gray-500 ยังไม่มีการสั่งซื้อ
 
+  .flex.flex-col.gap-y-4
+    ProductBanner(
+      v-for="item in computedItems"
+      :item="item"
+    )
+
 </template>
 
 <script lang="ts">
-import { defineComponent, Ref, ref } from '@nuxtjs/composition-api';
+import { computed, defineComponent, reactive, Ref, ref } from '@nuxtjs/composition-api';
 
 const purchase = defineComponent({
   layout: 'primary',
@@ -28,14 +36,26 @@ const purchase = defineComponent({
     this.$nuxt.$emit('setLayout', { color: true, profile: true, });
   },
   setup() {
-    const tab: Ref<Number> = ref(0);
-    const tabs: Ref<Array<String>> = ref(['ทั้งหมด','ที่ต้องชำระ','ที่ต้องจัดส่ง','ที่ต้องได้รับ','สำเร็จแล้ว','ยกเลิกแล้ว']);
+    const tab: Ref<any> = ref(0);
+    const tabs: Ref<Array<String>> = ref(['ทั้งหมด','ที่ต้องจัดส่ง','ที่ต้องได้รับ','สำเร็จแล้ว']);
     const tabSelect = (index: Number) => { tab.value = index };
+
+    const items = reactive([
+      { shopName: 'PetMall', name: 'อาหารสุนัข Woofs ขนาด 100g สำหรับพันธุ์เล็ก', option: 'รสตับ', amount: 1, price: 600, img: '/product/item/01.png', status: 'ที่ต้องจัดส่ง', date: '15-05-2022' },
+      { shopName: 'PetMall', name: 'อาหารสุนัข Woofs ขนาด 100g สำหรับพันธุ์เล็ก', option: 'รสตับ', amount: 1, price: 600, img: '/product/item/01.png', status: 'ที่ต้องได้รับ', date: '15-05-2022' },
+      { shopName: 'PetMall', name: 'อาหารสุนัข Woofs ขนาด 100g สำหรับพันธุ์เล็ก', option: 'รสตับ', amount: 1, price: 600, img: '/product/item/01.png', status: 'ที่ต้องได้รับ', date: '15-05-2022' },
+      { shopName: 'PetMall', name: 'อาหารสุนัข Woofs ขนาด 100g สำหรับพันธุ์เล็ก', option: 'รสตับ', amount: 1, price: 600, img: '/product/item/01.png', status: 'สำเร็จแล้ว', date: '15-05-2022' },
+    ])
+
+    const computedItems = computed(() => tab.value === 0 ? items : items.filter(item => item.status === tabs.value[tab.value]));
 
     return {
       tab,
       tabs,
       tabSelect,
+
+      items,
+      computedItems,
     };
   },
 });
