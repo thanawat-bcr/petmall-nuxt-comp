ad25e91fa94ca3ed923cd9b1bbdaf866dad66581
thanawat.bcr <thanwat.bcr@gmail.com>
2022-05-17 11:27:53 +0700

    [ADD] cart item component

--- components/product/Cart.vue
+++ components/product/Cart.vue
@@ -1,36 +1,39 @@
 <template lang="pug">
 .cart.grid-container.py-4
   .col-span-4.flex.items-center
-    SoCheckbox
+    SoCheckbox(v-model="item.selected")
     .flex.gap-x-4
-      img.w-20.h-20.object-contain(src="/product/item/01.png")
+      img.w-20.h-20.object-contain(:src="item.img")
       .flex.flex-col.gap-y-1
-        .text-sm.text-gray-500 {{ 'อาหารสุนัข Woofs ขนาด 100g สำหรับพันธุ์เล็ก' }}
+        .text-sm.text-gray-500 {{ item.name }}
         SoTag
   .col-span-1.flex.flex-col.gap-y-1
     .flex.items-center.justify-between.cursor-pointer
       .text-xs.text-gray-500 ตัวเลือกสินค้า
       i.ph-caret-down.text-xs.text-gray-500
-    .text-xs.text-gray-500 {{ 'รสไก่' }}
+    .text-xs.text-gray-500 {{ item.option }}
   .col-span-2.justify-end
-    .text-md.text-black.text-right ฿{{ '300' }}
+    .text-md.text-black.text-right ฿{{ item.price }}
   .col-span-2.justify-end.flex
-    .h-6.w-6.border.border-gray-200.flex.items-center.justify-center.cursor-pointer
+    .h-6.w-6.border.border-gray-200.flex.items-center.justify-center.cursor-pointer(@click="$emit('amountDecrease', item.id)")
       i.ph-minus.text-xs.text-gray-500
     .h-6.w-8.border.border-gray-200.flex.items-center.justify-center
-      .text-sm.text-gray-500 {{ '2' }}
-    .h-6.w-6.border.border-gray-200.flex.items-center.justify-center.cursor-pointer
+      .text-sm.text-gray-500 {{ item.amount }}
+    .h-6.w-6.border.border-gray-200.flex.items-center.justify-center.cursor-pointer(@click="$emit('amountIncrease', item.id)")
       i.ph-plus.text-xs.text-gray-500
   .col-span-2.flex.justify-end
-    .text-md.font-bold.text-orange-900.text-right ฿{{ '600' }}
+    .text-md.font-bold.text-orange-900.text-right ฿{{ item.price*item.amount }}
   .col-span-1.flex.justify-center
-    .button-lg.text-gray-400.underline แก้ไข
+    .button-lg.text-gray-400.underline ลบ
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api';
 
 const Cart = defineComponent({
+  props: {
+    item: {},
+  },
   // setup() {},
 });
 
--- pages/cart.vue
+++ pages/cart.vue
@@ -15,12 +15,17 @@
       .col-span-1
         h6.text-gray-500.text-center ตัวเลือก
 
-    .grid-container
-      ProductCart
+    .flex.flex-col.gap-y-4
+      ProductCart(
+        v-for="item in items"
+        :item="item"
+        @amountDecrease="amountDecrease"
+        @amountIncrease="amountIncrease"
+      )
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "@nuxtjs/composition-api";
+import { defineComponent, reactive, ref } from "@nuxtjs/composition-api";
 
 const cart = defineComponent({
   layout: 'secondary',
@@ -36,8 +41,33 @@ const cart = defineComponent({
   setup() {
     const selectAll = ref(false);
 
+    const items = reactive([
+      { id: 1, shopName: 'PetMall', name: 'อาหารสุนัข Woofs ขนาด 100g สำหรับพันธุ์เล็ก', option: 'รสตับ', amount: 2, price: 300, img: '/product/item/01.png', status: 'ที่ต้องจัดส่ง', date: '15-05-2022', selected: false },
+      { id: 2, shopName: 'PetMall', name: 'อาหารสุนัข Woofs ขนาด 100g สำหรับพันธุ์เล็ก', option: 'รสตับ', amount: 2, price: 600, img: '/product/item/02.png', status: 'ที่ต้องได้รับ', date: '15-05-2022', selected: false },
+      { id: 3, shopName: 'PetMall', name: 'อาหารสุนัข Woofs ขนาด 100g สำหรับพันธุ์เล็ก', option: 'รสตับ', amount: 6, price: 200, img: '/product/item/03.png', status: 'ที่ต้องได้รับ', date: '15-05-2022', selected: false },
+      { id: 4, shopName: 'PetMall', name: 'อาหารสุนัข Woofs ขนาด 100g สำหรับพันธุ์เล็ก', option: 'รสตับ', amount: 1, price: 400, img: '/product/item/01.png', status: 'สำเร็จแล้ว', date: '15-05-2022', selected: false },
+    ])
+
+    const amountDecrease = (id: any) => {
+      const index = items.findIndex(item => item.id === id)
+      if (items[index].amount === 1) return
+      // TODO: Disable counter while update amount to Database
+      items[index].amount--;
+    }
+    const amountIncrease = (id: any) => {
+      const index = items.findIndex(item => item.id === id)
+      // TODO: Check available item stock before increase more amount
+      // TODO: Disable counter while update amount to Database
+      items[index].amount++;
+    }
+
     return {
       selectAll,
+
+      items,
+
+      amountDecrease,
+      amountIncrease,
     };
   }
 });
