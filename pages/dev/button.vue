81723c7cff3f60608fec7c061411bd7bb3de7aee
thanawat.bcr <thanwat.bcr@gmail.com>
2022-05-27 20:43:23 +0700

    [CLEAN]

--- pages/dev/button.vue
+++ pages/dev/button.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 .button.flex.flex-col.gap-y-4.container
-  SoButton(mode="main" size="md" leading="plus" trailing="arrow-right") Button
-  SoButton(mode="soft" size="lg" leading="plus" trailing="arrow-right" block) Button
-  SoButton(mode="outline" size="lg" leading="plus" trailing="arrow-right") Button
-  SoButton(mode="text" size="sm" leading="plus" trailing="arrow-right") Button
+  SoButton(mode="main" size="md" leading="plus" trailing="arrow-right") Button 1 
+  SoButton(mode="soft" size="lg" leading="plus" trailing="arrow-right" block) Button 2
+  SoButton(mode="outline" size="lg" leading="plus" trailing="arrow-right") Button 3
+  SoButton(mode="text" size="sm" leading="plus" trailing="arrow-right") Button 4
 </template>
 
 <script lang="ts">
