8b22df288520da7a60a1c2e27335571604fb03d8
thanawat.bcr <thanwat.bcr@gmail.com>
2022-06-24 17:20:34 +0700

    refactor

--- components/address/Checkout.vue
+++ components/address/Checkout.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
 .checkout.w-full.flex.px-6.py-4.rounded-lg.gap-x-4.bg-gray-100
-  SoRadio
   .flex.flex-col.gap-y-2
     .text-md.text-gray-500.font-bold {{ address.name }}
     .text-md.text-gray-500 {{ address.phone }}
-    .text-sm.text-gray-500.truncate {{ address.address }}
+    .text-sm.text-gray-500 {{ address.address }}
 </template>
 
 <script lang="ts">
