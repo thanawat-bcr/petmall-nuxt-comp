6391a570b74fe6702ccae64cd792da78b150111a
thanawat.bcr <thanwat.bcr@gmail.com>
2022-07-11 16:29:45 +0700

    feat: min-max-value rules

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import { ValidationProvider, ValidationObserver, extend } from 'vee-validate';
-import { required, email, max } from "vee-validate/dist/rules";
+import { required, email, max, max_value, min_value } from "vee-validate/dist/rules";
 
 extend("required", {
   ...required,
@@ -15,6 +15,16 @@ extend("email", {
 extend("max", {
   ...max,
   message: 'ต้องมีความยาวไม่เกิน {length} ตัวอักษร'
+});
+
+extend("maxValue", {
+  ...max_value,
+  message: 'ต้องไม่มากกว่า {max}'
+});
+
+extend("minValue", {
+  ...min_value,
+  message: 'ต้องไม่ต่ำกว่า {min}'
 });
 
 Vue.component('ValidationProvider', ValidationProvider);
