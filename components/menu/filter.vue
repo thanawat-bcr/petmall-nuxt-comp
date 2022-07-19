7c641613a8937d9a0e01acff1d7810c05d17a90b
thanawat.bcr <thanwat.bcr@gmail.com>
2022-07-19 16:41:22 +0700

    feat: filter in route query

--- components/menu/filter.vue
+++ components/menu/filter.vue
@@ -10,7 +10,7 @@ aside.filter.w-full.h-full.flex.flex-col.gap-y-6
             v-for="op in animalOptions"
             :key="op.value"
             v-model="op.selected"
-            @input="filterSelectHandler"
+            @input="submit"
           ) {{ op.value }}
 
       section.flex.flex-col.gap-y-2(v-if="filter.categories")
@@ -20,7 +20,7 @@ aside.filter.w-full.h-full.flex.flex-col.gap-y-6
             v-for="op in categoryOptions"
             :key="op.value"
             v-model="op.selected"
-            @input="filterSelectHandler"
+            @input="submit"
           ) {{ op.value }}
 
       section.flex.flex-col.gap-y-2
@@ -31,17 +31,19 @@ aside.filter.w-full.h-full.flex.flex-col.gap-y-6
             SoInput(
               type="number"
               v-model="price.from"
-              :placeholder="filter.price.from"
-              :rules="`maxValue:${price.to}`"
+              :placeholder="0"
+              :rules="`${price.to ? 'maxValue:' + price.to : ''}`"
             )
           .flex.gap-x-1.items-center
             .text-sm.text-gray-500 จนถึง:
             SoInput(
               type="number"
               v-model="price.to"
-              :placeholder="filter.price.to"
-              :rules="`minValue:${price.from}`"
+              :placeholder="0"
+              :rules="`${price.from ? 'minValue:' + price.from : ''}`"
             )
+        .flex.flex-col.gap-y-2.mt-6
+          SoButton(block size="sm" type="submit") ค้นหา
 
       section.flex.flex-col.gap-y-2
         h6.text-h6.text-gray-500 คะแนนรีวิว  (ขึ้นไป)
@@ -51,18 +53,16 @@ aside.filter.w-full.h-full.flex.flex-col.gap-y-6
             :key="star"
             :class="`${score < star ? 'ph-star' : 'ph-star-fill'}`"
             class="hover:text-warn-900"
-            @click="score = star"
-            @mouseover="score = star"
+            @click="score = star; submit();"
           )
-          i.text-gray-500.text-xl.cursor-pointer.ph-x.ml-auto.cursor-pointer(@click="score = 0")
+          i.text-gray-500.text-xl.cursor-pointer.ph-x.ml-auto.cursor-pointer(@click="score = 0; submit();")
       .flex.flex-col.gap-y-2.mt-6
-        SoButton(block size="sm" type="submit") ค้นหา
         SoButton(block size="sm" mode="outline" @click="clearFilter") ลบทั้งหมด
 
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onMounted, reactive, ref, useRoute, useRouter, useStore, watch, watchEffect } from '@nuxtjs/composition-api';
+import { computed, defineComponent, onMounted, reactive, ref, useRoute, useRouter, watchEffect } from '@nuxtjs/composition-api';
 import { Filter, DEFAULT_FILTER } from '@/type/filter'
 
 const filter = defineComponent({
@@ -71,63 +71,61 @@ const filter = defineComponent({
       type: Object as () => Filter,
       default: () => DEFAULT_FILTER,
     },
-    sort: {}
   },
   setup(props: any, ctx: any) {
     const animalOptions = ref([])
     const categoryOptions = ref([])
-
     const filtered = computed(() => ({
       animals: animalOptions.value?.filter((op: any) => op.selected).map((op: any) => op.value),
       categories: categoryOptions.value?.filter((op: any) => op.selected).map((op: any) => op.value),
     }));
-    const price = reactive({
-      from: 0, to: 0
-    })
-    const score = ref(0);
 
+    const price: any = reactive({
+      from: '', to: ''
+    })
+    const score: any = ref(0);
+
+    const route = useRoute();
     watchEffect(() => {
-      animalOptions.value = props.filter?.animals?.map((op: any) => ({value: op, name: op, selected: false}))
-      categoryOptions.value = props.filter?.categories?.map((op: any) => ({value: op, name: op, selected: false}))
-      price.from = props.filter.price.from;
-      price.to = props.filter.price.to;
+      // console.log(route.value.query)
+      const animals = (route.value.query.animals as string)?.split(',') || []
+      const categories = (route.value.query.categories as string)?.split(',') || []
+      const from = route.value.query.from
+      const to = route.value.query.to
+      const star = route.value.query.score || 0
+
+      animalOptions.value = props.filter?.animals?.map((op: any) => {
+        if (animals.findIndex((item: string) => item === op) > -1) return { value: op, name: op, selected: true }
+        return { value: op, name: op, selected: false }
+      })
+      categoryOptions.value = props.filter?.categories?.map((op: any) => {
+        if (categories.findIndex((item: string) => item === op) > -1) return { value: op, name: op, selected: true }
+        return { value: op, name: op, selected: false }
+      })
+
+      price.from = from;
+      price.to = to;
+      score.value = star;
     });
 
     const submit = () => {
-      ctx.emit('filter', {
-        filter: {
-          ...filtered.value,
-          price,
-          score: score.value,
-        },
-        sort: props.sort,
-      }
-      );
+      const filter = [
+        filtered.value.animals.length > 0 ? '&animals=' + filtered.value.animals : '',
+        filtered.value.categories.length > 0 ? '&categories=' + filtered.value.categories : '',
+        price.from ? '&from=' + price.from : '',
+        price.to ? '&to=' + price.to : '',
+        score.value > 0 ? '&score=' + score.value : '',
+      ].join('').replace('&', '?');
+
+      history.pushState({}, '', filter || route.value.path);
     }
 
     const clearFilter = () => {
       animalOptions.value = props.filter?.animals?.map((op: any) => ({value: op, name: op, selected: false}))
       categoryOptions.value = props.filter?.categories?.map((op: any) => ({value: op, name: op, selected: false}))
-      price.from = props.filter.price.from;
-      price.to = props.filter.price.to;
+      price.from = '';
+      price.to = '';
       score.value = 0;
-    }
-
-    const router = useRouter();
-    const route = useRoute();
-
-    onMounted(() => {
-      console.log(route.value.query);
-    })
-
-    const filterSelectHandler = () => {
-      const filter = [
-        filtered.value.animals.length > 0 ? '&animals=' + filtered.value.animals : '',
-        filtered.value.categories.length > 0 ? '&categories=' + filtered.value.categories : '',
-      ].join('').replace('&', '?');
-
-      if (filter) history.pushState({}, '', filter);
-      else history.pushState({}, '', '?empty');
     }
 
     return {
@@ -140,8 +138,6 @@ const filter = defineComponent({
 
       submit,
       clearFilter,
-
-      filterSelectHandler,
     };
   },
 });
